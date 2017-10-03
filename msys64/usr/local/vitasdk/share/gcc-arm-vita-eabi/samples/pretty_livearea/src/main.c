6312973a3d2a1e52b519bd17a5a8e22ffa3cfea5
zecoxao <zecoxao@hotmail.com>
2017-10-03 16:25:05 +0100

    rework of msys64

--- msys64/usr/local/vitasdk/share/gcc-arm-vita-eabi/samples/pretty_livearea/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <psp2/kernel/processmgr.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello, pretty world!\n");
-	sceKernelExitProcess(0);
-	return 0;
-}
--- msys64/usr/share/cmake-3.6.2/Modules/Platform/MSYS-GNU-Fortran.cmake
+++
@@ -1,2 +0,0 @@
-include(Platform/MSYS-GNU)
-__msys_compiler_gnu(Fortran)
